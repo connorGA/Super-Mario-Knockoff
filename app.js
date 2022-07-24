b7ceb766e7f4930a12bcda9aaf6b97c972fc5404
connorGA <ronnoccap@gmail.com>
2022-07-23 21:47:55 -0700

    classes framework

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
+console.log('hello');
 
+window.addEventListener('load', function(){
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 720;
+
+    class InputHandler {
+
+    }
+
+    class Player {
+
+    }
+
+    class Background {
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', function(e){
+                console.log(e);
+            });
+        }
+    }
+
+    class Enemy {
+
+    }
+
+    function handleEnemies() {
+
+    }
+
+    function displayStatusText() {
+
+    }
+
+
+    function animate(){
+
+    }
+
+
+
+
+
+});
