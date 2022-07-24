34f748ceba90361421d22a30f51212046426c301
connorGA <ronnoccap@gmail.com>
2022-07-24 14:52:14 -0700

    mario sprite inserted, no jump animation yet

--- app.js
+++ app.js
@@ -38,14 +38,16 @@ window.addEventListener('load', function(){
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
-            this.width = 200;
-            this.height = 200;
+            this.width = 210;
+            this.height = 300;
             this.x = 0;
             this.y = this.gameHeight - this.height;
             this.image = document.getElementById('playerImage');
             this.frameX = 0;
             this.frameY = 0;
             this.speed = 0;
+            this.vy = 0;
+            this.weight = 1;
 
         }
         draw(context){
@@ -55,24 +57,69 @@ window.addEventListener('load', function(){
                 this.width, this.height, this.x, this.y, this.width, this.height);
         }
         update(input){
-            // horizontal movement 
-            this.x += this.speed;
             if (input.keys.indexOf('ArrowRight') > -1) {
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft') > -1) {
                 this.speed = -5;
+            } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+                this.vy -= 32;
             } else {
                 this.speed = 0;
             }
+            // horizontal movement 
+            this.x += this.speed;
+            if (this.x < 0) this.x = 0;
+            else if (this.x > this.gameWidth - this.width) this.x = this.gameWidth - this.width
+            //vertical movement
+            this.y += this.vy;
+            if (!this.onGround()){
+                this.vy += this.weight;
+                this.frameY = 1;
+            } else {
+                this.vy = 0;
+                this.frameY = 0;
+            }
+            if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height
+        }
+        onGround(){
+            return this.y >= this.gameHeight - this.height;
         }
     }
 
     class Background {
-   
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.image = document.getElementById('backgroundImage');
+            this.x = 0;
+            this.y = 0;
+            this.width = 1500;
+            this.height = 815;
+            this.speed = 10;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y, this.width, this. height);
+            context.drawImage(this.image, this.x + this.width - this.speed, this.y, this.width, this. height);
+        }
+        update(){
+            this.x -= this.speed;
+            if(this.x < 0 - this.width) this.x = 0;
+        }
     }
 
     class Enemy {
-
+        constructor(gameWidth, gameHeight){
+            this.gameWidth = gameWidth;
+            this.gameHeight = gameHeight;
+            this.width = 160;
+            this.height = 119;
+            this.image = document.getElementById('enemyImage');
+            this.x = 0;
+            this.y = 0;
+        }
+        draw(context){
+            context.drawImage(this.image, this.x, this.y);
+        }
     }
 
     function handleEnemies() {
@@ -86,12 +133,18 @@ window.addEventListener('load', function(){
         
     const input = new InputHandler();
     const player = new Player(canvas.width, canvas.height);
+    const background = new Background(canvas.width, canvas.height);
+    const enemy1 = new Enemy(canvas.width, canvas.height);
     
 
     function animate(){
         ctx.clearRect(0,0,canvas.width, canvas.height);
+        background.draw(ctx);
+        background.update();
         player.draw(ctx);
         player.update(input);
+        enemy1.draw(ctx);
+       
         requestAnimationFrame(animate);
     }
     animate();
