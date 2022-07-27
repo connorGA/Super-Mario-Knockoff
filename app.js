a6c1d970b1af509135bf2923cb14a97039162dfb
connorGA <ronnoccap@gmail.com>
2022-07-26 21:25:43 -0700

    updated with notes, progress

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
 console.log('hello');
 
-window.addEventListener('load', function(){
-    const canvas = document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
+window.addEventListener('load', function(){                         //wrap whole game in this so JS waits for all assets like sprite sheets and images to load before it executes code
+    const canvas = document.getElementById('canvas1');              //assign canvas a variable and grab it off our page
+    const ctx = canvas.getContext('2d');                            //ctx = "context". instance of built-in canvas 2D api that holds all drawing methods and properties we will need to animate the game
     canvas.width = 800;
-    canvas.height = 720;
+    canvas.height = 720;                                            //canvas.width and canvas.height are adjusting the canvas box to the desired size
 
-    class InputHandler {
+    class InputHandler {                                            //InputHandler class will apply eventListeners to keyboard events and hold array of all currently active keys
         constructor(){
-            this.keys = [];
-            window.addEventListener('keydown', e => {
-                // console.log(e.key);
-                if ((   e.key === 'ArrowDown' || 
+            this.keys = [];                                         //this.keys property is set equal to empty array. Keys will be added and removed from array as they are pressed and released to keep track of multiple key presses
+            window.addEventListener('keydown', e => {               //place eventListener directly in constructor. This way when we create an instance of inputHandler class later, all eventlisteners will be automatically applied.
+                // console.log(e.key);                              //call back function on eventListener has access to this built in(keydown) event object; (e) = assigned variable name for event
+                if ((   e.key === 'ArrowDown' ||                    
                         e.key === 'ArrowUp' ||
-                        e.key === 'ArrowLeft' ||
+                        e.key === 'ArrowLeft' ||                    //these four checking to see if e.key input is one of our four specified directions
                         e.key === 'ArrowRight')
                 
-                    && this.keys.indexOf(e.key) === -1){
-                    this.keys.push(e.key);
+                    && this.keys.indexOf(e.key) === -1){            //this says if one of the above inputs is applied AND this input is not yet in our this.keys array, then push it into the array
+                    this.keys.push(e.key);                          //if above e.key value is equal to one of our directions(Down,Up,Left,Right), this will push the value into our this.keys array
                 }
             });
 
-            window.addEventListener('keyup', e => {
+            window.addEventListener('keyup', e => {                 //"keyup" event is used to deal with the event that keys are released
                 // console.log(e.key);
                 if (    e.key === 'ArrowDown' || 
                         e.key === 'ArrowUp' ||
                         e.key === 'ArrowLeft' ||
                         e.key === 'ArrowRight'){
-                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                    this.keys.splice(this.keys.indexOf(e.key), 1); // this says that if key that was released was one of four above e.key inputs, find index of that key inside our this.keys array and use splice to remove (1) element from that array, hence the 1 at the end of parentheses
                 }
-                
+                                                                    // both of our "keydown" & "keyup" eventlisteners work together to add and remove key inputs from our this.keys array as they are pressed and released
             });
         }
     }
 
-    class Player {
-        constructor(gameWidth, gameHeight){
-            this.gameWidth = gameWidth;
+    class Player {                                                  //Player class will define properties of player object. It will draw it, animate it, and update its position based on user input
+        constructor(gameWidth, gameHeight){                         //player object needs to be aware of game boundaries so pass in gameWidth and gameHeight as arguments
+            this.gameWidth = gameWidth;                             //then game.Width & game.Height converted to class properties
             this.gameHeight = gameHeight;
-            this.width = 220;
+            this.width = 220;                                       //this.width & this.height dictate the size of the frame that holds our player sprite
             this.height = 300;
-            this.x = 0;
-            this.y = this.gameHeight - this.height;
-            this.image = document.getElementById('playerImage');
+            this.x = 0;                                             // this.x cord moves player on horizontal axis and this.y moves player on vertical axis
+            this.y = this.gameHeight - this.height;                 //this makes sure our player stands at the bottom of our specified game area
+            this.image = document.getElementById('playerImage');    // grabbing our player sprite sheet and bringing it into the project
             this.frameX = 0;
             this.frameY = 0;
             this.speed = 0;
@@ -50,13 +50,13 @@ window.addEventListener('load', function(){
             this.weight = 1;
 
         }
-        draw(context){
-            context.fillStyle = 'White';
-            context.fillRect(this.x, this.y, this.width, this.height);
-            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, 
+        draw(context){                                                                            //takes context as an argument to specify which canvas we want to draw on
+            context.fillStyle = 'White';                                                          //this is so we can see rectangle for now. Makes it easier to play around with sizing and stuff
+            context.fillRect(this.x, this.y, this.width, this.height);                            //call built in fillRect method to create a rectangle that will represent our player
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height,    //built in drawImage method used to draw player image. Pass it this.image from above that we used to grab our sprite sheet.
                 this.width, this.height, this.x, this.y, this.width, this.height);
         }
-        update(input){
+        update(input){                                                                            //this update method is so we can move player around based on our user inputs
             if (input.keys.indexOf('ArrowRight') > -1) {
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft') > -1) {
@@ -88,7 +88,7 @@ window.addEventListener('load', function(){
         }
     }
 
-    class Background {
+    class Background {                                                                                          //Background class will handle endleslly scrolling backgrounds
         constructor(gameWidth, gameHeight) {
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
@@ -109,7 +109,7 @@ window.addEventListener('load', function(){
         }
     }
 
-    class Enemy {
+    class Enemy {                                                                //Enemy class will generate enemies
         constructor(gameWidth, gameHeight){
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
@@ -124,31 +124,31 @@ window.addEventListener('load', function(){
         }
     }
 
-    function handleEnemies() {
+    function handleEnemies() {                                                  //function is responsible for adding, animating, and removing enemies from the game
     
     }
 
-    function displayStatusText() {
+    function displayStatusText() {                                              //utility function that handles things like score and "gameover" message
 
     }
 
         
-    const input = new InputHandler();
-    const player = new Player(canvas.width, canvas.height);
+    const input = new InputHandler();                                           //instance of InputHandler class that will run all code inside constructor, so at this point the eventListener "keydown" is applied
+    const player = new Player(canvas.width, canvas.height);                     //instance of Player class. Our constuctor expects a gameWidth and gameHeight as arguments, so we will pass it the canvas.width and canvas.height we specified at the top. This keeps our player inside our canvas boundaries
     const background = new Background(canvas.width, canvas.height);
     const enemy1 = new Enemy(canvas.width, canvas.height);
     
 
-    function animate(){
-        ctx.clearRect(0,0,canvas.width, canvas.height);
+    function animate(){                                                        // will run 60 x per second updating and drawing our game over and over 
+        ctx.clearRect(0,0,canvas.width, canvas.height);                        //To show only current animation frame. This built in method will delete whole canvas between each animation loop. This prevents the canvas from leaving a trail, instead showing only current frame
         background.draw(ctx);
         background.update();
-        player.draw(ctx);
-        player.update(input);
+        player.draw(ctx);                                                      // this displays player by calling our "draw" method we wrote above. It expects "context" as an argument, so we pass it our "ctx" from the top 
+        player.update(input);                                                  // call our update method 
         enemy1.draw(ctx);
        
-        requestAnimationFrame(animate);
+        requestAnimationFrame(animate);                                        //built in method to make everything in our animate function loop. Pass in "animate", the name of its parent function, to make endless animation loop
     }
-    animate();
+    animate();                                                                  //call animate function to start endless loop
 
 });
